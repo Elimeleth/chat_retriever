ada6cafbeef0a4e8bec9a81995fa31655a3871ee
4FREEK <juan07@biyuyo.org>
2024-01-09 11:56:44 -0400

    added chat

--- src/chat_model.ts
+++ src/chat_model.ts
@@ -1,10 +1,11 @@
-import { ChatCohere } from "@langchain/cohere";
+import { ChatCohere, Cohere } from "@langchain/cohere";
 
-export const model = new ChatCohere({
+export const model = new Cohere({
   apiKey: process.env.COHERE_API_KEY, // Default
   maxRetries: 2,
   temperature: .6,
   model: 'command-light',
+  maxTokens: 100,
   onFailedAttempt: (err) => {
     console.log({  err })
   }
