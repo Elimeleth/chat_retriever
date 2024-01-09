2b27721e6960322aa70e746cdf69935cb5fc7481
4FREEK <juan07@biyuyo.org>
2024-01-09 12:01:20 -0400

    reduce temperature model

--- src/chat_model.ts
+++ src/chat_model.ts
@@ -3,10 +3,10 @@ import { ChatCohere, Cohere } from "@langchain/cohere";
 export const model = new Cohere({
   apiKey: process.env.COHERE_API_KEY, // Default
   maxRetries: 2,
-  temperature: .6,
+  temperature: .4,
   model: 'command-light',
-  maxTokens: 100,
+  maxTokens: 90,
   onFailedAttempt: (err) => {
-    console.log({  err })
+    console.log({ err })
   }
 });
