68948a5eb1b0fdeed640e0c3a4c534c8e1134fbb
4FREEK <juan07@biyuyo.org>
2024-01-16 14:40:00 -0400

    added layer bot

--- src/chatbot/flows/welcome.flow.ts
+++ src/chatbot/flows/welcome.flow.ts
@@ -1,4 +1,4 @@
 import bot from '@bot-whatsapp/bot'
 
-export default bot.addKeyword('hola', { regex: true, sensitive: false })
-.addAnswer('Un gusto tenerte de nuevo ¿Como puedo ayudarte el día de hoy 😀?')
+export default bot.addKeyword('hola')
+    .addAnswer('Un gusto tenerte de nuevo ¿Como puedo ayudarte el día de hoy 😀?')
