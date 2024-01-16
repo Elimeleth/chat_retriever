42395bdf9e4ae20a7df05055fb76dc60c0a85153
4FREEK <juan07@biyuyo.org>
2024-01-16 17:00:20 -0400

    added flow regex

--- src/chatbot/flows/welcome.flow.ts
+++ src/chatbot/flows/welcome.flow.ts
@@ -1,4 +1,4 @@
 import bot from '@bot-whatsapp/bot'
 
-export default bot.addKeyword('hola')
+export default bot.addKeyword('/^hola/', { regex: true, sensitive: false })
     .addAnswer('Un gusto tenerte de nuevo ¿Como puedo ayudarte el día de hoy 😀?')
